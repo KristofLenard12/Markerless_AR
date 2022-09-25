7a72a68167f862e9fc324e99a01a66d639c4ec5b
Christian Sejer <christian_sejer@hotmail.com>
2022-09-25 15:35:57 +0200

    added door open/close

---
+++ Assets/Interactor.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+using UnityEngine.InputSystem.HID;
+
+public class Interactor : MonoBehaviour
+{
+	public TextMeshProUGUI interactText;
+	private Camera cam;
+	public float rayDistance;
+	private int layerMask;
+	private Ray ray;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		cam = GetComponent<Camera>();
+		layerMask = LayerMask.GetMask("Interactible"); 
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		ShootRay();
+	}
+
+	private void ShootRay()
+	{
+		RaycastHit hit;
+		ray = new Ray(transform.position, cam.transform.forward);
+
+		if (Physics.Raycast(ray, out hit, rayDistance, layerMask))
+		{
+			Debug.Log("Hit: " + hit.collider.gameObject.name);
+			if (hit.transform.tag == "Interactible")
+			{
+				interactText.gameObject.SetActive(true);
+
+				GetInput(hit);
+			}
+			else
+			{
+				interactText.gameObject.SetActive(false);
+			}
+		}
+		else
+		{
+			interactText.gameObject.SetActive(false);
+		}
+	}
+
+	private void GetInput(RaycastHit hit)
+	{
+		if (Input.touchCount > 0)
+		{
+			Touch touch = Input.GetTouch(0);
+
+			if(touch.phase == TouchPhase.Began)
+			{	
+				ActivateAnimation(hit);
+			}
+		}
+		else if (Input.GetKeyDown(KeyCode.E))
+		{
+			ActivateAnimation(hit);
+		}
+	}
+
+	private void ActivateAnimation(RaycastHit hit)
+	{
+		var anim = hit.collider.GetComponent<Animator>();
+
+		if (anim == null)
+		{
+			Debug.LogError("Door has no animator");
+			return;
+		}
+
+		if (anim.GetBool("toOpen"))
+		{
+			anim.SetBool("toOpen", false);
+		}
+		else
+		{
+			anim.SetBool("toOpen", true);
+		}
+	}
+
+	private void OnDrawGizmos()
+	{
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireSphere(transform.position, rayDistance);
+	}
+}
