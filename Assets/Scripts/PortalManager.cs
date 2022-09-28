be1c092a86cd2f49e02ce567526619a32db32ced
Anett Bianka Barát <58390834+Kilgarra@users.noreply.github.com>
2022-09-28 21:11:47 +0200

    fix

--- Assets/Scripts/PortalManager.cs
+++ Assets/Scripts/PortalManager.cs
@@ -1,34 +1,40 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Rendering;
+using Debug = UnityEngine.Debug;
 
 public class PortalManager : MonoBehaviour
 {
     public GameObject MainCamera;
-
-    public GameObject Sponza;
-
-    private Material[] SponzaMaterials;
+    
+    public Material Walls;
+    public Material Chichen_Itza;
+    public Material Sphinx;
+    public Material Petra;
+    private List<Material> Materials;
     private static readonly int StencilComp = Shader.PropertyToID("_StencilComp");
 
     private bool triggerActive = false;
 
     void Start()
     {
-        SponzaMaterials = Sponza.GetComponent<Renderer>().sharedMaterials;
-        Debug.Log(SponzaMaterials[0]);
+        
+         Materials = GetComponent<Renderer>().materials.ToList();
+         Debug.Log(Materials);
     }
 
     private void Update()
     {
         if (!triggerActive)
         {
-            for (int i = 0; i < SponzaMaterials.Length; i++)
-            {
-                SponzaMaterials[i].SetInt(StencilComp, (int)CompareFunction.Equal);
-            }
+            Walls.SetInt(StencilComp, (int)CompareFunction.Equal);
+            Chichen_Itza.SetInt(StencilComp, (int)CompareFunction.Equal);
+            Sphinx.SetInt(StencilComp, (int)CompareFunction.Equal);
+            Petra.SetInt(StencilComp, (int)CompareFunction.Equal);
         }
     }
 
@@ -39,11 +45,10 @@ public class PortalManager : MonoBehaviour
 
         if (camPositionInPortalSpace.y < 0.5f)
         {
-            Debug.Log("here");
-            for (int i = 0; i < SponzaMaterials.Length; i++)
-            {
-                SponzaMaterials[i].SetInt(StencilComp, (int)CompareFunction.Always);
-            }
+            Walls.SetInt(StencilComp, (int)CompareFunction.Always);
+            Chichen_Itza.SetInt(StencilComp, (int)CompareFunction.Always);
+            Sphinx.SetInt(StencilComp, (int)CompareFunction.Always);
+            Petra.SetInt(StencilComp, (int)CompareFunction.Always);
         }
         
     }
